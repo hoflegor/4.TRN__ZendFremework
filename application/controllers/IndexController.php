ab82503eaa4ec95ca14499c2b78f3c2606780d52
thoffmann <thoffmann@unity-t.pl>
2019-01-18 16:27:28 +0100

    create edit & delete

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -17,12 +17,24 @@ class IndexController extends Zend_Controller_Action
 
 	public function addAction()
 	{
-		// action body
-		$form = new Application_Form_Album();
-		$form->submit->setLabel('Add');
 
-		$this->view->form = $form;
 
+		$form = new Application_Form_Album(); //    Inicjujemy obiekt klasy Form_Album
+		$form->submit->setLabel('Add');//   ustawiamy nazwę przycisku wysyłającego formularz na “Add”
+		$this->view->form = $form;//przypisujemy do zmiennej widoku.
+
+
+//		Jeśli metoda obiektu żądania, isPost() zwraca wartość true, znaczy to że formularz został wysłany,
+//dlatego też możemy pobrać przesłane dane za pomocą metody getPost() i sprawdzić ich poprawność za
+//pomocą metody isValid() formularza.
+//
+//		Jeśli przesłane dane są poprawne, inicjuemy obiekt klasy modelu -
+//Application_Model_DbTable_Albums i wywołujemy zdefniowaną wcześniej metodę addAlbum() w
+//celu zapisania danych w bazie.
+//
+//		Jeśli przesłane dane są poprawne, inicjuemy obiekt klasy modelu -
+//Application_Model_DbTable_Albums i wywołujemy zdefniowaną wcześniej metodę addAlbum() w
+//celu zapisania danych w bazie.
 		if ($this->getRequest()->isPost())
 		{
 			$formData = $this->getRequest()->getPost();
@@ -35,6 +47,8 @@ class IndexController extends Zend_Controller_Action
 				$albums->addAlbum($artist, $title);
 				$this->_helper->redirector('index');
 			}
+//			Jeśli dane formularza nie przeszły procesu walidacji, wypełniamy pola formularza przesłanymi danymi i
+//ponownie wyświetlamy go użytkownikowi.
 			else
 			{
 				$form->populate($formData);
@@ -44,12 +58,55 @@ class IndexController extends Zend_Controller_Action
 
 	public function editAction()
 	{
-		// action body
+
+		$form = new Application_Form_Album();
+		$form->submit->setLabel('Save');
+		$this->view->form = $form;
+
+		if ($this->getRequest()->isPost()){
+			$formData = $this->getRequest()->getPost();
+			if ($form->isValid($formData))
+			{
+				$id = (int)$form->getValue('id');
+				$artist = $form->getValue('artist');
+				$title = $form->getValue('title');
+				$albums = new Application_Model_DbTable_Albums();
+				$albums->updateAlbum($id, $artist, $title);
+
+				$this->_helper->redirector('index');
+			}
+			else{
+				$form->populate($formData);
+			}
+		}else{
+			$id = $this->_getParam('id', 0);
+
+			if ($id > 0)
+			{
+				$albums = new Application_Model_DbTable_Albums();
+				$form -> populate($albums->getAlbum($id));
+			}
+		}
+
+
 	}
 
 	public function deleteAction()
 	{
-		// action body
+		if ($this->getRequest()->isPost())
+		{
+			$del=$this->getRequest()->getPost('del');
+			if ($del == 'Yes'){
+				$id = $this->getRequest()->getPost('id');
+				$albums = new Application_Model_DbTable_Albums();
+				$albums->deleteAlbum($id);
+			}
+			$this->_helper->redirector('index');
+		}else{
+			$id=$this->getParam('id', 0);
+			$albums = new Application_Model_DbTable_Albums();
+			$this->view->album = $albums->getAlbum($id);
+		}
 	}
 
 
